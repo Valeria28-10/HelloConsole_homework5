f7fae0172330e06489e4f9156163eced882ea815
Valeria28-10 <valeriakonovalenko828@gmail.com>
2023-05-26 18:45:55 +0300

    ДЗ_5 Задача 2

---
+++ Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] GenerateArray(int Lenght)
+{
+    int[] array = new int[Lenght];
+    for(int i = 0; i < Lenght; i++)
+    {
+        array[i] = new Random().Next(-100, 101);
+    }
+    Console.WriteLine(String.Join(",", array));
+    return array;
+}
+int lenght = 7;
+int[] array = GenerateArray(lenght);
+int sumOddPosition = 0;
+for(int i = 0; i < lenght; i++)
+{
+    if(i % 2 != 0)
+    {
+        sumOddPosition += array[i];
+    }
+}
+Console.WriteLine($"Cумма элементов, стоящих на нечётных позициях: {sumOddPosition}");
