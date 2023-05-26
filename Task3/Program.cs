ac37c55681d9cd11d86eda6f00be2d9d114b125b
Valeria28-10 <valeriakonovalenko828@gmail.com>
2023-05-26 19:21:01 +0300

    ДЗ_5 Задача 3

---
+++ Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между 
+// максимальным и минимальным элементов массива.
+// Диапазон [-10, 10]. Обратите внимание на вещественных чисел
+// Для примера возьмём диапазон побольше. [3 7 22 2 78] -> 76
+
+double[] GenerateArray(int Lenght)
+{
+    double[] array = new double[Lenght];
+    for(int i = 0; i < Lenght; i++)
+    {
+        array[i] = new Random().Next(-10, 11);
+    }
+    Console.WriteLine(String.Join(",", array));
+    return array;
+}
+
+double[] array = GenerateArray(10);
+double minValue = array[0];
+double maxValue = array[0];
+for (int i = 0; i < 10; i++)
+{
+    if(maxValue < array[i]) maxValue = array[i];
+    else if(minValue > array[i]) minValue = array[i]; 
+}
+Console.WriteLine($"max = {maxValue}, min = {minValue}");
+Console.WriteLine($"Разница между максимальным и минимальным элементами массива: {maxValue - minValue}");
